351e493dfa0c5a1beb0376307ee12aad82e09bd4
alex.gong <alex.gong@akqa.com>
2013-09-16 16:45:23 +0800

    new framework

---
+++ js/common.js
@@ -0,0 +1,9 @@
+document.write(
+    '<script src="/js/3rdparty/jquery-1.9.1.min.js"></script>' +
+    '<script src="/js/3rdparty/modernizr-2.6.2.min.js"></script>' +
+    '<script src="/js/3rdparty/jquery.slides.min.js"></script>' +
+    '<script src="/js/3rdparty/jquery.easing.1.3"></script>' +
+    '<script src="/js/3rdparty/jail.js"></script>' +
+    '<script src="/js/3rdparty/require.js"></script>' +
+    '<script src="/js/main.js"><\/script>'
+);
